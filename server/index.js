f54e84d5dfdc3dd4d5df27aa2388334f658c358b
Parfait Vertie <parfaitv@il-h1.msk.21-school.ru>
2023-06-29 14:13:18 +0300

    :rocket: release

---
+++ server/index.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+const sequelize = require('./db')
+const express = require('express')
+const models = require('./models/model')
+const cors = require('cors')
+const router = require('./routes/index')
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
+const fileUplod = require('express-fileupload')
+const path = require('path')
+
+
+
+const PORT = process.env.PORT || 5000
+
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(fileUplod({}))
+app.use('/api', router)
+
+
+// error handling
+app.use(errorHandler)
+
+
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
